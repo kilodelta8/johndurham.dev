b71f8e99f20252c06efee6f7b7df2fdc9e5d3802
John Durham <john@MacBookPro.lan>
2025-03-20 04:21:36 -0400

    macdev branched, started login/signup

---
+++ src/components/Resume.tsx
@@ -0,0 +1,106 @@
+
+function Resume(){
+    return (
+        <main>
+            <div>
+                <h1 style={{paddingBottom: "3px", marginBottom: "3px", paddingTop: "50px"}}>John Durham</h1>
+                <h4 style={{paddingTop: "3px", marginTop: "3px"}}>www.johndurham.dev  |  github.com/kilodelta8  |  linkedin.com/in/johnsdurham/</h4>
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingBottom: "1px", paddingTop: "15px"}}>OBJECTIVE</h2>
+                <p style={{paddingTop: "1px"}}>
+                I am a highly skilled and experienced professional seeking a position in the
+                software industry. My expertise in manufacturing environments, software
+                development, leadership, supervision, and organization makes me a valuable asset to
+                any team. I am looking for a position that will allow me to utilize my skills and
+                experience to help a company grow and succeed.
+                </p>
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>SKILLS</h2>
+                <ul style={{listStyleType: "none", margin: "0"}}>
+                    <li style={{paddingTop: "1px"}}><i><b>Software Development</b></i>: AAS in Software Development and experience in developing applications.</li>
+                    <li style={{paddingTop: "1px"}}><i><b>Leadership</b></i>: Technical Supervisor experience, managing teams of 21+ employees.</li>
+                    <li style={{paddingTop: "1px"}}><i><b>Supervision</b></i>: Experience in supervising and mentoring teams.</li>
+                    <li style={{paddingTop: "1px"}}><i><b>Organization</b></i>: Highly organized and detail-oriented.</li>
+                    <li style={{paddingTop: "1px"}}><i><b>Communication</b></i>: Excellent written and verbal communication skills.</li>
+                    <li style={{paddingTop: "1px"}}><i><b>Problem Solving</b></i>: Ability to analyze and resolve complex problems.</li>
+                </ul>
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>PROFESSIONAL EXPERIENCE</h2>
+                Order Entry Technician, OmniCare Pharmacy, Englewood, OH
+                March 2009 - May2015
+                • Processed orders and prescriptions and provided customer service
+                • Operated medical software and answered phones
+                • Supported nurses, doctors and other medical staff
+                Technical Supervisor, Fuyao Glass America, Moraine, OH
+                February 2015 - June 2022
+                • Managed ARG bending production lines, including over 21 employees.
+                • Facilitated product changes in molds and other items quickly and efficiently.
+                • Led and attended meetings to maintain current and pertinent operating knowledge.
+                • Met KPI objectives and performance in alignment with company and department goals.
+                • Troubleshot, diagnosed, repaired, or escalated machine issues as needed.
+                Machining Operator, Dmax LTD, Brookville, OH
+                June 2022- October 2023
+                • Interpreted results and measurements daily per machine and initiated corrective action.
+                • Properly maintained tools, gauges, workstation and utility equipment in a safe and
+                efficient manner.
+                • Inspected and performed routine maintenance on machines and equipment with limited
+                guidance.
+                • Responsible for general cleaning of the assigned work cell including equipment.
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>EDUCATION</h2>
+                AAS: Software Development
+                Expected August 2025*
+                
+                Launch Code LC101 Full Stack Web Development, Memphis, TN
+                Full Stack Web Development Certificate
+                May 2019
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>RELEVANT COURSEWORK</h2>
+                React with Redux Master Course
+                API design and implementation
+                Introduction to Problem Solving and Computer Programming
+                Network Fundamentals
+                C++ Software Development
+                Web Site Development with HTML and CSS
+                Java Software Development I
+                Small Group Communication
+                Management and Organizational Behavior
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>TECHNICAL SKILLS & ABILITIES</h2>
+                • Creates professional documents, correspondence, spreadsheets, and
+                presentations.
+                • Designs and builds web applications using React and Redux.
+                • Knowledgeable in management and organizational behavior for effective remote
+                work.
+                • Develops algorithms, flowcharts, and pseudocode using sequence, selection, and
+                iteration.
+                • Designs web pages that enhance usability and interactivity.
+                • Analyzes problem sets and generates solutions using software.
+                • Maintains clear and consistent communication with colleagues, clients, and
+                leadership.
+                • Produced a basic calculator with a GUI using Java and JavaFX.
+                • Developed CRUD applications using Python, Flask, and MySQL, and built a
+                blogging website.
+                • Designed CRUD applications using Java, SpringBoot, React and MySQL, and built a
+                job search website.
+                • Led a team of aspiring and learning students in building robotics applications.
+                • Assisted fellow students in learning and retaining programming skills.
+                • Demonstrates strong organizational skills and professionalism in remote work
+                environments.
+            </div>
+            <div>
+                <h2 style={{textDecoration: "underline", paddingTop: "15px", paddingBottom: "1px"}}>CERTIFICATIONS</h2>
+                <img src="./src/assets/UdemyRestAPICert.jpg" alt="Udemy API Cert" style={{width: "550px", height: "400px", padding: "5px"}}/>
+                <img src="./src/assets/lfs101.png" alt="LFS101 Cert" style={{width: "550px", height: "400px", padding: "5px"}}/>
+            </div>
+        </main>
+    );
+};
+
+export default Resume;
